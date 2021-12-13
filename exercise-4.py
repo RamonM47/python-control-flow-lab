df655de574d21cfb88e4cbc7b91544b085005bb7
RamonM47 <aviramon.mesa@gmail.com>
2021-12-13 17:30:59 -0500

    finish all 6 tasks for lab

---
+++ exercise-4.py
@@ -0,0 +1,25 @@
+# exercise-04 What kind of Triangle?
+
+# Write the code that:
+# 1. Prompts the user to enter the three lengths of a triangle (one at a time):
+#      Enter the lengths of three side of a triangle:
+#      a: 
+#      b:
+#      c:
+# 2. Write the code that determines if the triangle is:
+#      equalateral - all three sides are equal in length
+#      scalene - all three sides are unequal in length
+#      isosceles - two sides are the same length
+# 3. Print a message such as:
+#      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+
+a = input ('side a:')
+b = input ('side b:')
+c = input ('side c:')
+
+if a == b and b == c:
+    print("equalateral - all three sides are equal in length")
+elif a != b and b != c:
+    print("scalene - all three sides are unequal in length")
+else:
+    print("isosceles - two sides are the same length")
